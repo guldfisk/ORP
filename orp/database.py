72e88737c666401dbf5b90eea4a710035d693d60
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2020-01-14 16:03:43 +0100

    xd nice

--- orp/database.py
+++ orp/database.py
@@ -9,238 +9,238 @@ from orp import relationships as _relationships
 
 
 class Incrementer(object):
-	
-	def __init__(self, initial_value: int = 0):
-		self.value = initial_value
-	
-	def __call__(self):
-		value = self.value
-		self.value = value + 1
-		return value
+
+    def __init__(self, initial_value: int = 0):
+        self.value = initial_value
+
+    def __call__(self):
+        value = self.value
+        self.value = value + 1
+        return value
 
 
 class Key(object):
-	
-	def __init__(
-		self,
-		target: str,
-		calc_value: 't.Callable[[Key, Model, dict], t.Any]' = None,
-		input_values: t.Iterable[str] = None,
-	):
-		self._target = target
-		self.calc_value = calc_value
-		self.input_values = input_values
-	
-	@property
-	def target(self):
-		return self._target
-	
-	@property
-	def private_target(self):
-		return '_'+self._target
-	
-	def __repr__(self):
-		return self._target
+
+    def __init__(
+        self,
+        target: str,
+        calc_value: 't.Callable[[Key, Model, dict], t.Any]' = None,
+        input_values: t.Iterable[str] = None,
+    ):
+        self._target = target
+        self.calc_value = calc_value
+        self.input_values = input_values
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def private_target(self):
+        return '_'+self._target
+
+    def __repr__(self):
+        return self._target
 
 
 class PrimaryKey(object):
-	
-	def __init__(self, keys: t.Union[t.Tuple[t.Union[Key, str], ...], Key, str]):
-		self.key = (
-			tuple(item if isinstance(item, Key) else Key(item) for item in keys)
-			if isinstance(keys, tuple) else
-			keys if isinstance(keys, Key) else Key(keys)
-		)
-	
-	def __get__(self, instance, owner):
-		if instance is None:
-			return self.key
 
-		if isinstance(self.key, tuple):
-			return tuple(
-				getattr(instance, key.target) for key in self.key
-			)
+    def __init__(self, keys: t.Union[t.Tuple[t.Union[Key, str], ...], Key, str]):
+        self.key = (
+            tuple(item if isinstance(item, Key) else Key(item) for item in keys)
+            if isinstance(keys, tuple) else
+            keys if isinstance(keys, Key) else Key(keys)
+        )
 
-		return getattr(instance, self.key.target)
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self.key
+
+        if isinstance(self.key, tuple):
+            return tuple(
+                getattr(instance, key.target) for key in self.key
+            )
+
+        return getattr(instance, self.key.target)
 
 
 class ForeignKey(Key):
-	
-	def __init__(self, target: str, foreign_target: str):
-		super().__init__(target)
-		self._foreign_target = foreign_target
-	
-	@property
-	@abstractmethod
-	def relationship(self):
-		pass
-	
-	@property
-	def foreign_target(self):
-		return self._foreign_target
+
+    def __init__(self, target: str, foreign_target: str):
+        super().__init__(target)
+        self._foreign_target = foreign_target
+
+    @property
+    @abstractmethod
+    def relationship(self):
+        pass
+
+    @property
+    def foreign_target(self):
+        return self._foreign_target
 
 
 class ForeignOne(ForeignKey):
-	
-	@property
-	def relationship(self):
-		return _relationships.One
+
+    @property
+    def relationship(self):
+        return _relationships.One
 
 
 class Model(object):
-	primary_key = PrimaryKey(
-		Key('id', calc_value=lambda k, o, m: o._INCREMENTER()),
-	)
-	_INCREMENTER = Incrementer()
-	
-	def __new__(cls, *args, **kwargs):
-		keys = (
-			cls.primary_key
-			if isinstance(cls.primary_key, tuple) else
-			(cls.primary_key,)
-		)
+    primary_key = PrimaryKey(
+        Key('id', calc_value=lambda k, o, m: o._INCREMENTER()),
+    )
+    _INCREMENTER = Incrementer()
 
-		for key, arg in zip(
-			chain(
-				*(
-					(filtered_key.target,)
-					if filtered_key.input_values is None else
-					filtered_key.input_values
-					for filtered_key in keys
-					if filtered_key.calc_value is None or filtered_key.input_values is not None
-				)
-			),
-			args,
-		):
-			kwargs[key] = arg
+    def __new__(cls, *args, **kwargs):
+        keys = (
+            cls.primary_key
+            if isinstance(cls.primary_key, tuple) else
+            (cls.primary_key,)
+        )
 
-		obj = cls._unlinked_foreign_new(kwargs)
+        for key, arg in zip(
+            chain(
+                *(
+                    (filtered_key.target,)
+                    if filtered_key.input_values is None else
+                    filtered_key.input_values
+                    for filtered_key in keys
+                    if filtered_key.calc_value is None or filtered_key.input_values is not None
+                )
+            ),
+            args,
+        ):
+            kwargs[key] = arg
 
-		for key in keys:
-			if isinstance(key, ForeignKey):
-				try:
-					getattr(obj, key.private_target).set(
-						kwargs[key.target],
-						ignore_previous_value = True,
-					)
-				except KeyError:
-					getattr(obj, key.private_target).set(
-						key.calc_value(key, obj, kwargs),
-						ignore_previous_value = True,
-					)
+        obj = cls._unlinked_foreign_new(kwargs)
 
-		return obj
-	
-	@classmethod
-	def _unlinked_foreign_new(cls, key_map):
-		obj = super().__new__(cls)
-		keys = (
-			cls.primary_key
-			if isinstance(cls.primary_key, tuple) else
-			(cls.primary_key,)
-		)
+        for key in keys:
+            if isinstance(key, ForeignKey):
+                try:
+                    getattr(obj, key.private_target).set(
+                        kwargs[key.target],
+                        ignore_previous_value = True,
+                    )
+                except KeyError:
+                    getattr(obj, key.private_target).set(
+                        key.calc_value(key, obj, kwargs),
+                        ignore_previous_value = True,
+                    )
 
-		for key in keys:
-			if isinstance(key, ForeignOne):
-				setattr(
-					obj,
-					key.private_target,
-					key.relationship(
-						obj,
-						key.foreign_target,
-					)
-				)
-				try:
-					getattr(obj, key.private_target).join_with(key_map[key.target])
-				except KeyError:
-					getattr(obj, key.private_target).join_with(key.calc_value(key, obj, key_map))
-			else:
-				try:
-					setattr(obj, key.private_target, key_map[key.target])
+        return obj
 
-				except KeyError:
-					setattr(obj, key.private_target, key.calc_value(key, obj, key_map))
+    @classmethod
+    def _unlinked_foreign_new(cls, key_map):
+        obj = super().__new__(cls)
+        keys = (
+            cls.primary_key
+            if isinstance(cls.primary_key, tuple) else
+            (cls.primary_key,)
+        )
 
-		return obj
-	
-	def __eq__(self, other):
-		return (
-			isinstance(other, self.__class__)
-			and self.primary_key == other.primary_key
-		)
-	
-	def __hash__(self):
-		return hash((self.__class__, self.primary_key))
-	
-	def __repr__(self):
-		return f'{self.__class__.__name__}({self.primary_key})'
+        for key in keys:
+            if isinstance(key, ForeignOne):
+                setattr(
+                    obj,
+                    key.private_target,
+                    key.relationship(
+                        obj,
+                        key.foreign_target,
+                    )
+                )
+                try:
+                    getattr(obj, key.private_target).join_with(key_map[key.target])
+                except KeyError:
+                    getattr(obj, key.private_target).join_with(key.calc_value(key, obj, key_map))
+            else:
+                try:
+                    setattr(obj, key.private_target, key_map[key.target])
 
-	# @classmethod
-	# def _new_with_primary_key(cls, values):
-	# 	return cls._unlinked_foreign_new(values)
-	
-	def __reduce__(self):
-		return (
-			self._unlinked_foreign_new,
-			(
-				{
-					key.target:
-						getattr(self, key.target)
-					for key in
-					self.__class__.primary_key
-				}
-				if isinstance(self.__class__.primary_key, tuple) else
-				{
-					self.__class__.primary_key.target:
-						getattr(self, self.__class__.primary_key.private_target),
-				},
-			),
-			self.__dict__,
-		)
+                except KeyError:
+                    setattr(obj, key.private_target, key.calc_value(key, obj, key_map))
+
+        return obj
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__)
+            and self.primary_key == other.primary_key
+        )
+
+    def __hash__(self):
+        return hash((self.__class__, self.primary_key))
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.primary_key})'
+
+    def __reduce__(self):
+        return (
+            self._unlinked_foreign_new,
+            (
+                {
+                    key.target:
+                        getattr(self, key.target)
+                    for key in
+                    self.__class__.primary_key
+                }
+                if isinstance(self.__class__.primary_key, tuple) else
+                {
+                    self.__class__.primary_key.target:
+                        getattr(self, self.__class__.primary_key.private_target),
+                },
+            ),
+            self.__dict__,
+        )
 
 
-class Table(t.Dict):
-	
-	def insert(self, item):
-		self.__setitem__(item.primary_key, item)
+K = t.TypeVar('K')
+V = t.TypeVar('V', bound = Model)
+
+
+class Table(t.Dict[K, V]):
+
+    def insert(self, item):
+        self.__setitem__(item.primary_key, item)
 
 
 class Database(object):
 
-	def __init__(self, tables: t.Dict[t.Type[Model], Table]):
-		self._tables = tables
+    def __init__(self, tables: t.Dict[t.Type[Model], Table]):
+        self._tables = tables
 
-		hasher = hashlib.sha256()
-		for model_type_name, table in sorted(
-			(
-				(model_type.__name__, table)
-				for model_type, table in
-				tables.items()
-			)
-		):
-			hasher.update(
-				pickle.dumps(
-					(
-						model_type_name,
-						sorted(table.keys()),
-					)
-				)
-			)
+        hasher = hashlib.sha256()
+        for model_type_name, table in sorted(
+            (
+                (model_type.__name__, table)
+                for model_type, table in
+                tables.items()
+            )
+        ):
+            hasher.update(
+                pickle.dumps(
+                    (
+                        model_type_name,
+                        sorted(table.keys()),
+                    )
+                )
+            )
 
-		self._checksum = hasher.digest()
+        self._checksum = hasher.digest()
 
-	@property
-	def checksum(self) -> t.ByteString:
-		return self._checksum
+    @property
+    def checksum(self) -> t.ByteString:
+        return self._checksum
 
-	def __getitem__(self, model: t.Type[Model]) -> Table:
-		return self._tables.__getitem__(model)
+    def __getitem__(self, model: t.Type[Model]) -> Table:
+        return self._tables.__getitem__(model)
 
-	def __iter__(self) -> t.Iterable[Table]:
-		return self._tables.values()
+    def __iter__(self) -> t.Iterable[Table]:
+        return self._tables.values()
 
-	def __copy__(self):
-		return self
+    def __copy__(self):
+        return self
 
-	def __deepcopy__(self, memodict={}):
-		return self
+    def __deepcopy__(self, memodict):
+        return self
--- orp/relationships.py
+++ orp/relationships.py
@@ -8,157 +8,157 @@ T = t.TypeVar('T')
 
 class Relationship(t.Generic[T], metaclass=ABCMeta):
 
-	def __init__(self, owner, target_field: str):
-		self._owner = owner
-		self.target_field = target_field
+    def __init__(self, owner, target_field: str):
+        self._owner = owner
+        self.target_field = target_field
 
-	@property
-	def owner(self):
-		return self._owner
+    @property
+    def owner(self):
+        return self._owner
 
-	@staticmethod
-	def from_owner(owner):
-		return owner
+    @staticmethod
+    def from_owner(owner):
+        return owner
 
-	@abstractmethod
-	def join_with(self, one: T) -> None:
-		pass
+    @abstractmethod
+    def join_with(self, one: T) -> None:
+        pass
 
-	@abstractmethod
-	def disjoint_with(self, one: T) -> None:
-		pass
+    @abstractmethod
+    def disjoint_with(self, one: T) -> None:
+        pass
 
-	@classmethod
-	def _create_new(cls):
-		return super().__new__(cls)
+    @classmethod
+    def _create_new(cls):
+        return super().__new__(cls)
 
-	def __reduce__(self) -> tuple:
-		return self._create_new, (), self.__dict__
+    def __reduce__(self) -> tuple:
+        return self._create_new, (), self.__dict__
 
 
 class Many(Relationship, t.AbstractSet[T]):
 
-	def __init__(self, owner, target_field: str):
-		super().__init__(owner, target_field)
-		self._many = set() #type: t.Set[T]
+    def __init__(self, owner, target_field: str):
+        super().__init__(owner, target_field)
+        self._many: t.Set[T] = set()
 
-	def join_with(self, one: T):
-		self._many.add(one)
+    def join_with(self, one: T):
+        self._many.add(one)
 
-	def add(self, one: T):
-		getattr(self.from_owner(one), self.target_field).join_with(self.owner)
-		self.join_with(one)
+    def add(self, one: T):
+        getattr(self.from_owner(one), self.target_field).join_with(self.owner)
+        self.join_with(one)
 
-	def update(self, other: t.Iterable[T]):
-		for item in other:
-			self.add(item)
+    def update(self, other: t.Iterable[T]):
+        for item in other:
+            self.add(item)
 
-	def disjoint_with(self, one: T):
-		self._many.remove(one)
+    def disjoint_with(self, one: T):
+        self._many.remove(one)
 
-	def remove(self, one: T):
-		getattr(self.from_owner(one), self.target_field).join_with(None)
-		self.disjoint_with(one)
+    def remove(self, one: T):
+        getattr(self.from_owner(one), self.target_field).join_with(None)
+        self.disjoint_with(one)
 
-	def __repr__(self) -> str:
-		return self._many.__repr__()
+    def __repr__(self) -> str:
+        return self._many.__repr__()
 
-	def __iter__(self) -> t.Iterator[T]:
-		return self._many.__iter__()
+    def __iter__(self) -> t.Iterator[T]:
+        return self._many.__iter__()
 
-	def __bool__(self) -> bool:
-		return bool(self._many)
+    def __bool__(self) -> bool:
+        return bool(self._many)
 
-	def __len__(self) -> int:
-		return self._many.__len__()
+    def __len__(self) -> int:
+        return self._many.__len__()
 
-	def __contains__(self, item) -> bool:
-		return self._many.__contains__(item)
+    def __contains__(self, item) -> bool:
+        return self._many.__contains__(item)
 
-	def __sub__(self, other) -> t.AbstractSet[T]:
-		return self._many.__sub__(other)
+    def __sub__(self, other) -> t.AbstractSet[T]:
+        return self._many.__sub__(other)
 
-	def __or__(self, other) -> t.AbstractSet:
-		return self._many.__or__(other)
+    def __or__(self, other) -> t.AbstractSet:
+        return self._many.__or__(other)
 
 
 class ListMany(Relationship, t.List[T]):
 
-	def __init__(self, owner, target_field: str):
-		super().__init__(owner, target_field)
-		self._many = [] #type: t.List[T]
+    def __init__(self, owner, target_field: str):
+        super().__init__(owner, target_field)
+        self._many: t.List[T] = []
 
-	def join_with(self, one: T):
-		self._many.append(one)
+    def join_with(self, one: T):
+        self._many.append(one)
 
-	def add(self, one: T):
-		getattr(self.from_owner(one), self.target_field).join_with(self.owner)
-		self.join_with(one)
+    def add(self, one: T):
+        getattr(self.from_owner(one), self.target_field).join_with(self.owner)
+        self.join_with(one)
 
-	def disjoint_with(self, one: T):
-		self._many.remove(one)
+    def disjoint_with(self, one: T):
+        self._many.remove(one)
 
-	def remove(self, one: T):
-		getattr(self.from_owner(one), self.target_field).join_with(None)
-		self.disjoint_with(one)
+    def remove(self, one: T):
+        getattr(self.from_owner(one), self.target_field).join_with(None)
+        self.disjoint_with(one)
 
-	def clear(self) -> None:
-		for item in list(self._many):
-			self.remove(item)
+    def clear(self) -> None:
+        for item in list(self._many):
+            self.remove(item)
 
-	def append(self, one: T) -> None:
-		self.add(one)
+    def append(self, one: T) -> None:
+        self.add(one)
 
-	def extend(self, ones: t.Iterable[T]) -> None:
-		for one in ones:
-			self.add(one)
+    def extend(self, ones: t.Iterable[T]) -> None:
+        for one in ones:
+            self.add(one)
 
-	def __len__(self) -> int:
-		return self._many.__len__()
+    def __len__(self) -> int:
+        return self._many.__len__()
 
-	def __iter__(self) -> t.Iterator[T]:
-		return self._many.__iter__()
+    def __iter__(self) -> t.Iterator[T]:
+        return self._many.__iter__()
 
-	def __getitem__(self, i: int) -> T:
-		return self._many.__getitem__(i)
+    def __getitem__(self, i: int) -> T:
+        return self._many.__getitem__(i)
 
-	def __contains__(self, one: T) -> bool:
-		return self._many.__contains__(one)
+    def __contains__(self, one: T) -> bool:
+        return self._many.__contains__(one)
 
 
 class One(Relationship, t.Generic[T]):
 
-	def __init__(self, owner, target_field: str, initial_value: T = None):
-		super().__init__(owner, target_field)
-		self._one = None
-		self.set(initial_value)
+    def __init__(self, owner, target_field: str, initial_value: T = None):
+        super().__init__(owner, target_field)
+        self._one = None
+        self.set(initial_value)
 
-	def get(self) -> T:
-		return self._one
+    def get(self) -> T:
+        return self._one
 
-	def join_with(self, many: T):
-		self._one = many
+    def join_with(self, many: T):
+        self._one = many
 
-	def disjoint_with(self, many: T):
-		self._one = None
+    def disjoint_with(self, many: T):
+        self._one = None
 
-	def set(self, many: T, ignore_previous_value = False):
-		if self._one is not None and not ignore_previous_value:
-			getattr(self.from_owner(self._one), self.target_field).disjoint_with(self.owner)
+    def set(self, many: T, ignore_previous_value = False):
+        if self._one is not None and not ignore_previous_value:
+            getattr(self.from_owner(self._one), self.target_field).disjoint_with(self.owner)
 
-		if many is not None:
-			getattr(self.from_owner(many), self.target_field).join_with(self.owner)
+        if many is not None:
+            getattr(self.from_owner(many), self.target_field).join_with(self.owner)
 
-		self.join_with(many)
+        self.join_with(many)
 
 
 class OneDescriptor(t.Generic[T]):
 
-	def __init__(self, field: str):
-		self.field = field
+    def __init__(self, field: str):
+        self.field = field
 
-	def __get__(self, instance, owner) -> T:
-		return getattr(instance, self.field).get()
+    def __get__(self, instance, owner) -> T:
+        return getattr(instance, self.field).get()
 
-	def __set__(self, instance, value: T):
-		getattr(instance, self.field).set(value)
+    def __set__(self, instance, value: T):
+        getattr(instance, self.field).set(value)
